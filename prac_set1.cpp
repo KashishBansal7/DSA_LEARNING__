295f58e8cb705671b42b0fde3e9ff8723d443c04
KashishBansal7 <kashishbansal002@gmail.com>
2025-09-05 18:27:10 +0530

    loop

--- prac_set1.cpp
+++ prac_set1.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
-int main(){
-    for(int j=1;j<5;j++){
-        for(int i=1;i<5;i++){
-            cout<<"*";
-        }
-         cout<<endl;
-    }
+// int main(){
+//     for(int j=1;j<=5;j++){
+//         for(int i=1;i<=5;i++){
+//             cout<<i<<" ";
+//         }
+//          cout<<endl;
+//     }
     
 
-    return 0;
+//     return 0;
+// }
+int main(){
+    for(int row=1;row<6;row++)
+        {   for(int col=1;col<6;col++){
+             cout<<row<<"   ";
+        }
+        cout<<endl;
+    }
 }
