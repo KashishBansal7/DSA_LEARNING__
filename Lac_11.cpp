d762ff9da32f65851a427ab09b0b8675fd5bd9a3
KashishBansal7 <kashishbansal002@gmail.com>
2025-09-19 22:44:58 +0530

    Lac_11.cpp

---
+++ Lac_11.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int i=1; // initialization
+//     while(i<=10)  //break condition
+//{
+//         cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+//         i++; // updation
+//     }
+    
+// }
+
+// int main(){
+// int n;
+// cout<<"Enter a number : ";
+// cin>>n;
+// int i=1;
+//     while(i<=n){
+//         if(n%i==0)
+//         cout<<i<<endl;
+//         i++;
+
+//     }
+    
+//   }    
+// int main(){
+//     int n;
+//     cout<<"Enter a number : ";
+//     cin>>n;
+//     while(n%2==0){
+//         cout<<"even"<< " ";
+//         break;
+//     }
+
+// }
+
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter a number : ";
+//     cin>>n;
+//     while(n%2!=0){
+//         cout<<"odd"<<" ";
+
+//         break;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"Enter a number : ";
+//     cin>>n;
+//     while(n%2==0){
+//         cout<<"even"<<" ";
+//         break;
+//     }
+//     while(n%2!=0){
+//         cout<<"odd"<<" ";
+//         break;
+//     }
+// }
+
+
+// DO WHILE LOOP 
+
+// int main(){
+//     int n;
+//     cout<<" Enter a number : ";
+//     cin>>n;
+//     int i=1; // initialization
+//     do{
+//         cout<<i<<endl;
+//         i++; // updation
+//     }
+//     while(i<=n); // break condition
+
+// }
--- prac_set1.cpp
+++ prac_set1.cpp
@@ -11,11 +11,93 @@ using namespace std;
 
 //     return 0;
 // }
-int main(){
+
+
+/*int main(){
     for(int row=1;row<6;row++)
         {   for(int col=1;col<6;col++){
              cout<<row<<"   ";
         }
         cout<<endl;
     }
+}*/
+
+/*int main()
+{
+    int row,col;
+
+    for(row=1;row<=5;row=row+1)
+    {  
+        for(col=5;col>=1;col=col-1)
+        {
+             cout<<col<<"   ";
+        }
+        cout<<endl;
+    }
+}*/
+
+/*int main()
+{
+    int row,col;
+    for(row=1;row<=5;row=row+1)
+    { 
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<row*col<<" ";
+        }
+        cout<<endl;
+    }
+}*/
+/*int main()
+{
+    int row,col;
+    for(row=1;row<=5;row=row+1)
+    { 
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<row*col<<" ";
+        }
+        cout<<endl;
+    }
+}*/
+
+/*int main()
+{
+    int row,col;
+    for(row=1;row<=5;row=row+1)
+    { 
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<< col*col*col<<" ";
+        }
+        cout<<endl;
+    }
+}*/
+
+/*int main()
+{
+    int row,col;
+    for(row=1;row<=5;row=row+1)
+    { 
+        char name='A'+row-1;
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<name<<" ";
+        }
+        cout<<endl;
+    }
+}*/
+
+int main()
+{
+    int row;
+    for(row=1;row<=5;row=row+1)
+    { 
+        for(char col=1;col<=5;col=col-1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
 }
+
